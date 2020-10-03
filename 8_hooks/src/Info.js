02110e94bcddde8f79ae6af24a8756ef8498124a
HoduGwaja <honeybutter9250@gmail.com>
2020-10-03 21:53:37 +0900

    [commit | 2020.10.03 | added useEffect]

--- 8_hooks/src/Info.js
+++ 8_hooks/src/Info.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Info = () => {
     const [name, setName] = useState(' ');
     const [nickname, setNickname] = useState(' ');
-
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다!')
+        console.log({
+            name,
+            nickname
+        })
+    })
     const onChangename = e => {
         setName(e.target.value);
     };
@@ -11,7 +17,6 @@ const Info = () => {
     const onChangeNickname = e => {
         setNickname(e.target.value);
     };
-
     return(
         <div>
             <div>
@@ -35,4 +40,9 @@ export default Info;
 /* 
 * useState 여러번 사용하기
 * 관리할 상태를 여러개 만들면 된다
+*/
+
+/*
+* useEffect
+* 리엑트 컴포넌트가 렌더링 될때마다 특정 작업을 수행하도록 설정할 수 있는 Hook
 */
