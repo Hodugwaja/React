6b469242958a9dc12aae96412230c206b48a658a
HoduGwaja <honeybutter9250@gmail.com>
2020-10-19 18:57:24 +0900

    [commit | 2020.10.19 | added provider]

--- 15_contextapi/src/App.js
+++ 15_contextapi/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
 const App = () => {
   return (
-    <div>
-        <ColorBox />
-    </div>
+    <ColorContext.Provider value = {{Color : 'red'}}>
+      <div>
+          <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 }
 
 export default App;
+
+
+//Provider를 사용하면 context내의 value를 변경할 수 있음
